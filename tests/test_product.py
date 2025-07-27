bd4f8d3a63a3c569233883271be82823f5aafcd0
Алексей Коновалов <lex-sorokin@yandex.ru>
2025-07-28 01:59:13 +0500

    homework_14_1

---
+++ tests/test_product.py
@@ -0,0 +1,8 @@
+from src.product import Product
+
+
+def test_product_init(first_product: Product) -> None:
+    assert first_product.name == "Iphone 15"
+    assert first_product.description == "512GB, Gray space"
+    assert first_product.price == 210000.0
+    assert first_product.quantity == 8
